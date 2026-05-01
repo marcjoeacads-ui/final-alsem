35f003ca6b5f5f8f0a81b203a7e4f84141f18426
marcjoeacads-ui <marcjoeacads@gmail.com>
2026-05-01 23:24:39 +0800

    added some helpful kinda intrusive notes

--- matrix.asm
+++ matrix.asm
@@ -1,18 +1,20 @@
 .data
     
-    r: .asciiz "Enter n rows"
-    c: .asciiz "Enter n columns"
+    prmrow: .asciiz "Enter n rows: "
+    prmcol: .asciiz "Enter n columns: "
 
 
 
     # prompts for entering matrixs values
-    startp: .asciiz "Enter Number for ["
-    midp: .asciiz "]["
-    endp: .asciiz "]: "
+    prmstart: .asciiz "Enter Number for ["
+    prmmid: .asciiz "]["
+    prmend: .asciiz "]: "
+    #this is for printing the matrix "Enter Number for [0][0]: " and so on..
 
 
-    title: .asciiz "\n--- Current Matrix State ---\n"
-    tb: .asciiz "\t"
+    #output formatting
+    title: .asciiz "\n--- Current Matrix State ---\n" # header
+    tb: .asciiz "\t"  #special characters for printing
     nl: .asciiz "\n"
 
 
@@ -21,84 +23,83 @@
 
 main: 
 
-    #Rows
-    li $v0, 4
-    la $a0, r
+    #Rows print
+    li $v0, 4 
+    la $a0, prmrow
     syscall
 
+    #Rows input
     li $v0, 5
     syscall
     move $s6, $v0
 
-    #Columns
+    #Columns print
     li $v0, 4
-    la $a0, c
+    la $a0, prmcol
     syscall
 
+    #Columns input
     li $v0, 5
     syscall
     move $s3, $v0
 
-    #gets rows x columns
-    mul $s1, $s6, $s3 
-    
+    #Calculate memory n allocate
+    mul $s1, $s6, $s3 #s1 = ttl numbers of m( r x c )
     mul $a0, $s1, 4 #a0 = total bytes since for floats/int we need 4 bytes each
 
     li $v0, 9
     syscall
 
-    move $s5, $v0 
+    move $s5, $v0 # base address of the matrix in memory
     move $s0, $s5 
 
     li $s2, 0
 
-inputl:
-    beq $s2, $s1, endi
+inputrequest:
+    beq $s2, $s1, endinputrequest 
 
     div $s2, $s3
-    mflo $t0
-    mfhi $t1
-
+    mflo $t0 #mflo gives us the quotient which is the row number
+    mfhi $t1 #gives us the column number
+    
+    # Print the prompt for the current element
     li $v0, 4
-    la $a0, startp
+    la $a0, prmstart #[
     syscall
-
     li $v0, 1
     move $a0, $t0
     syscall
-
-    li $v0, 4
-    la $a0, midp
+    li $v0, 4 # first number 
+    la $a0, prmmid #[]
     syscall
-
-    li $v0, 1
+    li $v0, 1 # second number
     move $a0, $t1
     syscall
-
-
     li $v0, 4
-    la $a0, endp
+    la $a0, prmend #]:  
     syscall
 
     li $v0, 6 #reads float
     syscall
 
-    swc1 $f0, 0($s0) 
+    swc1 $f0, 0($s0)  # store the input value in the matrix
 
-    addi $s0, $s0, 4
+    # Increment the pointer and the counter
+    addi $s0, $s0, 4 
     addi $s2, $s2, 1
-    j inputl
+    j inputrequest
 
-endi:
+endinputrequest:
+    # Print the title for the matrix
     li $v0, 4
     la $a0, title
     syscall
-
+    # Reset $s0 to the base address of the matrix for printing
     move $s0, $s5
     li $s2, 0
 
 
-loopp: 
+loopforprinting: 
     beq $s2, $s1, EOM # stands for end of matrix
 
     lwc1 $f12, 0($s0)
@@ -114,16 +115,15 @@ loopp:
 
     div $s2, $s3
     mfhi $t0
-    bnez $t0, loopp # if not end of row, continue printing
+    bnez $t0, loopforprinting # if not end of row, continue printing
 
     # Print a newline after each row
     li $v0, 4
     la $a0, nl
     syscall
 
-    j loopp
+    j loopforprinting   
 
 EOM:
-    # Exit the program
     li $v0, 10
     syscall
