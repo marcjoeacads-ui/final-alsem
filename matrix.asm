32a83687732e2be715def138c2166126a6e2084e
marcjoeacads-ui <marcjoeacads@gmail.com>
2026-05-02 22:09:26 +0800

    added Visuals but no operations yet

--- matrix.asm
+++ matrix.asm
@@ -1,52 +1,47 @@
-.data
-    
-    prmrow: .asciiz "Enter n rows: "
+.data # variables and constants
+    prmrow: .asciiz "Enter n rows: " 
     prmcol: .asciiz "Enter n columns: "
     
 .text
 .globl main
-
 main: 
-
-    #Rows print
-    li $v0, 4 
+    # Rows input
+    li $v0, 4  
     la $a0, prmrow
     syscall
-
-    #Rows input
     li $v0, 5
     syscall
     move $s6, $v0
 
-    #Columns print
+    # Columns input
     li $v0, 4
     la $a0, prmcol
     syscall
-    
-    #Columns input
     li $v0, 5
     syscall
     move $s3, $v0
 
-    #Calculate memory n allocate
-    mul $s1, $s6, $s3 #s1 = ttl numbers of m( r x c )
-    mul $a0, $s1, 4 #a0 = total bytes since for floats/int we need 4 bytes each
+    # Calculate memory and allocate
+    mul $s1, $s6, $s3 
+    mul $a0, $s1, 4 
 
-    li $v0, 9
+    li $v0, 9 # allocates memory and zeros it out
     syscall
-    move $s5, $v0 # base address of the matrix in memory
+    move $s5, $v0 
 
-    move $a0, $s5
-    move $a1, $s1
-    move $a2, $s3
-    jal inputmatrix 
+    # Call input which displays in real-time
+    move $a0, $s5 #base address
+    move $a1, $s1 #rowcount
+    move $a2, $s3 #colcount
 
+    jal inputmatrix  
+
+    # Final display one last time
     move $a0, $s5
     move $a1, $s1
     move $a2, $s3
     jal displaymatrix
 
-EOM:
     li $v0, 10
     syscall
 
--- visual.asm
+++ visual.asm
@@ -1,37 +1,51 @@
 .data
-    
-
     prmstart: .asciiz "Enter Number for ["
     prmmid: .asciiz "]["
     prmend: .asciiz "]: "
 
-  #output formatting
-    title: .asciiz "\n--- Current Matrix State ---\n" # header
-    tb: .asciiz "\t"  #special characters for printing
+    title: .asciiz "\n--- Current Matrix State ---\n"
+    tb: .asciiz "\t"
     nl: .asciiz "\n"
+
 .text
 
 inputmatrix:
-    move $t0, $a0
-    move $t1, $a1
-    move $t2, $a2
-    li $t3, 0 
+    # Save $ra and $s registers to stack (since we are calling another function)
+    addi $sp, $sp, -20 
+    sw $ra, 16($sp)
+    sw $s0, 12($sp) # Base address
+    sw $s1, 8($sp)  # Total elements
+    sw $s2, 4($sp)  # Column count
+    sw $s3, 0($sp)  # Counter (i)
+
+    move $s0, $a0
+    move $s1, $a1
+    move $s2, $a2
+    li $s3, 0 
 
 inputloop:
-    beq $t3, $t1, endinputloop
+    beq $s3, $s1, endinputloop
 
-    div $t3, $t2
-    mflo $t4 #row number
-    mfhi $t5 #column number
+    # Refresh the screen every time
+    move $a0, $s0
+    move $a1, $s1
+    move $a2, $s2
+    jal displaymatrix 
 
+    # Calculate row/col for the prompt
+    div $s3, $s2
+    mflo $t4 # row
+    mfhi $t5 # col
+
+    # 3. Print the prompt
     li $v0, 4
-    la $a0, prmstart
+    la $a0, prmstart #[
     syscall
     li $v0, 1
     move $a0, $t4
     syscall
     li $v0, 4
-    la $a0, prmmid
+    la $a0, prmmid #][
     syscall
     li $v0, 1
     move $a0, $t5
@@ -40,31 +54,49 @@ inputloop:
     la $a0, prmend
     syscall
 
+    # 4. Read float and store
     li $v0, 6
     syscall
-    swc1 $f0, 0($t0) #store the input value in the matrix
+    
+    # Calculate offset: counter * 4
+    sll $t6, $s3, 2   # $t6 = $s3 * 4
+    add $t6, $t6, $s0 # add base address
+    swc1 $f0, 0($t6)
 
-    addi $t0, $t0, 4 #move to the next element in the matrix
-    addi $t3, $t3, 1 #increment the counter
+    addi $s3, $s3, 1 # increment counter
     j inputloop
 
-   
 endinputloop:
-    jr $ra #jr returns to the caller function (main)
+    # Restore stack and return
+    lw $ra, 16($sp)
+    lw $s0, 12($sp)
+    lw $s1, 8($sp)
+    lw $s2, 4($sp)
+    lw $s3, 0($sp)
+    addi $sp, $sp, 20
+    jr $ra
 
 displaymatrix:
-    move $t0, $a0
-    move $t1, $a1
-    move $t2, $a2
-    li $t3, 0
+    # 
+    addi $sp, $sp, -4
+    sw $ra, 0($sp)
+    
+    # Move arguments to temps for the display logic
+    move $t0, $a0 # base
+    move $t1, $a1 # total
+    move $t2, $a2 # cols
+    
+    jal clear_screen
 
     li $v0, 4
     la $a0, title
     syscall
+
+    li $t3, 0 # local counter
 displayloop:
     beq $t3, $t1, enddisplayloop
 
-    lwc1 $f12, 0($t0) #load the current element of the matrix
+    lwc1 $f12, 0($t0)
     li $v0, 2
     syscall
 
@@ -72,17 +104,32 @@ displayloop:
     la $a0, tb
     syscall 
 
-    addi $t0, $t0, 4 #move to the next element in the matrix
+    addi $t0, $t0, 4
     addi $t3, $t3, 1 
 
-    div  $t3, $t2        # Divide counter by columns
-    mfhi $t4             # Get remainder
-    bnez $t4, displayloop # If remainder is  !0, don't 
+    div  $t3, $t2
+    mfhi $t4
+    bnez $t4, displayloop 
 
     li $v0, 4
     la $a0, nl
-    syscall # print a newline
+    syscall
     j displayloop
 
 enddisplayloop:
+    lw $ra, 0($sp)
+    addi $sp, $sp, 4
     jr $ra 
+
+clear_screen:
+    li $t7, 0
+    li $t8, 20 # 20 to ensure is at bottom
+clear_loop:
+    beq $t7, $t8, end_clear
+    li $v0, 4
+    la $a0, nl
+    syscall
+    addi $t7, $t7, 1
+    j clear_loop
+end_clear:
+    jr $ra
